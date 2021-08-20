dfd26c5d839535175eb895271f77baaf21002fa8
Mary Saray <msaray.ms@outlook.com>
2021-08-19 18:30:46 -0700

    Created Data folder and added ProductContext file in it

---
+++ AspnetCoreWithBugs/Data/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspnetCoreWithBugs.Models;
+
+namespace AspnetCoreWithBugs.Data
+{
+    public class ProductContext : DbContext
+    {
+        // constructor
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+            // default this is empty
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
